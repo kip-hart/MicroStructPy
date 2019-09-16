e29d54e988a2aa6905c0bce2eb22145bc867fa49
Kip Hart <kiphart91@gmail.com>
2019-09-16 18:25:15 -0400

    completed the 3D foam example

--- examples/foam_3D.py
+++ examples/foam_3D.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import microstructpy as msp
 import numpy as np
 import scipy.stats
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
@@ -22,25 +23,31 @@ def main():
                 'size': 2
                 }
 
-    void_a = 0.4 * domain.volume
+    void_a = 0.57 * domain.volume
     void_seeds = msp.seeding.SeedList.from_info(void_mat, void_a)
-    void_seeds.position(domain, rtol=0, verbose=True)
+    void_seeds.position(domain, rtol=0.1, verbose=True)
     void_tess = msp.meshing.PolyMesh.from_seeds(void_seeds, domain)
 
-    # Add Foam
+    # Add Foam to Edges in Void Tessellation
     foam_mat = {'material_type': 'amorphous',
                 'shape': 'sphere',
                 'size': 0.2
                 }
-    foam_r1 = 0.1
+    foam_r1 = 0.10
     foam_r2 = 0.05
 
     edge_seeds = {}
     kps_w_seeds = set()
+    ext_kps = set()
+    for neighs, facet in zip(void_tess.facet_neighbors, void_tess.facets):
+        if np.min(neighs) < 0:
+            ext_kps |= set(facet)
+
+
     for neighs, facet in zip(void_tess.facet_neighbors, void_tess.facets):
         if np.min(neighs) < 0:
             continue
-        for i, kp_end in enumeratet(facet):
+        for i, kp_end in enumerate(facet):
             kp_beg = facet[i - 1]
             if kp_beg < kp_end:
                 edge = (kp_beg, kp_end)
@@ -48,62 +55,80 @@ def main():
                 edge = (kp_end, kp_beg)
             if edge in edge_seeds:
                 continue
+            if edge[0] in ext_kps or edge[1] in ext_kps:
+                continue
                     
             pt1 = np.array(void_tess.points[edge[0]])
             pt2 = np.array(void_tess.points[edge[1]])
             edge_len = np.linalg.norm(pt2 - pt1)
             x, r = seed_edge(foam_r1, foam_r2, edge_len)
 
-            # TODO: continue adding foam seeds
+            if edge[0] in kps_w_seeds:
+                x = x[1:]
+                r = r[1:]
+            if edge[1] in kps_w_seeds:
+                x = x[:-1]
+                r = r[:-1]
+            kps_w_seeds |= set(edge)
 
-            if edge_len < foam_r1:
-                edge_seeds[edge] = msp.seeding.SeedList([])
-                continue
+            seeds = []
+            for xi, ri in zip(x, r):
+                f = xi / edge_len
+                cen = (1 - f) * pt1 + f * pt2
+                geom = msp.geometry.Sphere(r=ri, center=cen)
+                seed = msp.seeding.Seed(geom, phase=1, position=cen)
+                seeds.append(seed)
+            edge_seeds[edge] = seeds
 
-            n_pts = int(edge_len / foam_r1) + 1
-            f = np.linspace()
-
-
-    foam_seeds = [msp.seeding.Seed(msp.geometry.Sphere(r=foam_r1, phase=1)) for
-             pt in void_tess.points]
-    for s, x in zip(foam_seeds, void_tess.points):
-        s.position = x
+    all_edge_seeds = [s for edge in edge_seeds for s in edge_seeds[edge]]
+    foam_seeds = msp.seeding.SeedList(all_edge_seeds)
 
     # Combine Results
     for seed in void_seeds:
-        seed.geometry.r *= 0.7
+        seed.geometry.r *= 1 - 0.0
 
     materials = [void_mat, foam_mat]
     seeds = void_seeds + foam_seeds
+    print('polygonal meshing')
     pmesh = msp.meshing.PolyMesh.from_seeds(seeds, domain)
 
     # Triangular Mesh
-    tmesh = msp.meshing.TriMesh.from_polymesh(pmesh,
-                                              materials,
-                                              min_angle=20,
-                                              max_edge_length=0.1)
+    print('triangular meshing')
+    tmesh = msp.meshing.TriMesh.from_polymesh(pmesh, materials)
 
     # Plot
+    print('plotting')
+    fig = plt.figure()
+    ax = fig.gca(projection=Axes3D.name)
+    ax._axis3don = False
+
     tmesh.plot(facecolor='aquamarine',
-               edgecolor='k',
-               linewidth=0.2)
+               edgecolor='gray',
+               linewidth=0.1)
 
-    plt.show()
-    '''
+    lims = domain.limits
+    for x in lims[0]:
+        for y in lims[1]:
+            for z in lims[2]:
+                ax.plot([x], [y], [z])
+    ax.set_aspect('equal')
 
-    plt.gca().set_position([0, 0, 1, 1])
-    plt.axis('image')
-    plt.gca().set_axis_off()
-    plt.gca().get_xaxis().set_visible(False)
-    plt.gca().get_yaxis().set_visible(False)
+    fname = os.path.join(dirname, 'trimesh.png')
+    plt.savefig(fname, bbox_inches='tight', pad_inches=0)
 
-    xlim, ylim = domain.limits
-    plt.axis([xlim[0], xlim[1], ylim[0], ylim[1]])
+    # Remove Whitespace from Plot
+    foam_image = plt.imread(fname)
+    c_min = foam_image[:,:,:-1].min(axis=-1)
+    min0 = c_min.min(axis=0)
+    mask0 = min0 < 1
+    ax0 = np.nonzero(mask0)[0][[0, -1]]
+    
+    min1 = c_min.min(axis=1)
+    mask1 = min1 < 1
+    ax1 = np.nonzero(mask1)[0][[0, -1]]
 
-    for ext in ['png', 'pdf']:
-        fname = os.path.join(dirname, 'trimesh.' + ext)
-        plt.savefig(fname)
-    '''
+    cr_image = foam_image[ax1[0]:ax1[1], ax0[0]:ax0[1]]
+    plt.imsave(fname.replace('.png', '_cropped.png'), cr_image)
 
 
 def seed_edge(r1, r2, len_x):
@@ -124,43 +149,11 @@ def seed_edge(r1, r2, len_x):
     xs = np.concatenate((x_arr, len_x - np.flip(x_arr)))
     rs = np.concatenate((r_arr, np.flip(r_arr)))
 
-    if len(xs) > 0:
-        xs = np.append(xs, x_max)
-        rs = np.append(rs, r2)
+    if len(xs) > 0 and x_max - x_arr[-1] > r2:
+        xs = np.insert(xs, -1, x_max)
+        rs = np.insert(rs, -1, r2)
 
     return xs, rs
-'''
-def pick_edge(void_tess):
-    f_neighs = void_tess.facet_neighbors
-    i = -1
-    neighs = [-1, -1]
-    while any([n < 0 for n in neighs]):
-        i = np.random.randint(len(f_neighs))
-        neighs = f_neighs[i]
-    facet = void_tess.facets[i]
-    j = np.random.randint(len(facet))
-    kp1 = facet[j]
-    kp2 = facet[j - 1]
-    return kp1, kp2
-
-def trial_position(void_tess):
-    kp1, kp2 = pick_edge(void_tess)
-    pt1 = void_tess.points[kp1]
-    pt2 = void_tess.points[kp2]
-
-    f = np.random.rand()
-    return [f * x1 + (1 - f) * x2 for x1, x2 in zip(pt1, pt2)]
-
-
-def check_pt(point, r, breakdowns):
-    pts = breakdowns[:, :-1]
-    rads = breakdowns[:, -1]
-
-    rel_pos = pts - point
-    dist = np.sqrt(np.sum(rel_pos * rel_pos, axis=1))
-    min_dist = rads + r - 0.2 * np.minimum(rads, r)
-    return np.all(dist > min_dist)
-'''
 
 
 if __name__ == '__main__':
