656ff29d673d58074ae7c44d595cf4021c964ec3
Kip Hart <kiphart91@gmail.com>
2019-09-18 14:55:33 -0400

    added logos back to _static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,10 +111,10 @@ html_theme = 'alabaster'
 # documentation.
 #
 html_theme_options = {
-    'logo': '../../examples/logo/pad_logo.svg',
+    'logo': 'logo/pad_logo.svg',
     'logo_name': True,
     'sidebar_width': '230px',
-    'touch_icon': '../../examples/logo/logo.svg',
+    'touch_icon': 'logo/logo.svg',
     'description': 'Microstructure modeling, mesh generation, analysis, and visualization.',
     'analytics_id': 'UA-147258715-1',
 }
@@ -133,12 +133,12 @@ html_short_title = 'MSP'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '../../examples/logo/favicon.ico'
+html_favicon = 'logo/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '../../examples/logo']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
