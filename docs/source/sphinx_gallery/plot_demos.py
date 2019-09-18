afd784cb4305df030486502f673d23a83584c03c
Kip Hart <kiphart91@gmail.com>
2019-09-18 15:06:28 -0400

    Revert "commented out XML demos"
    This reverts commit 5685403febb6886a143aea7c093c61ef2d12e291.

--- docs/source/sphinx_gallery/plot_demos.py
+++ docs/source/sphinx_gallery/plot_demos.py
@@ -16,14 +16,13 @@ locale.setlocale(locale.LC_NUMERIC, "C")
 
 example_dir = '../../../examples'
 
-'''
 # Run XML files
 xml_pattern = os.path.join(example_dir, '*.xml')
 for filename in glob.glob(xml_pattern):
     msp.cli.run_file(filename)
-'''
 
 # Run Python Scripts
 py_pattern = os.path.join(example_dir, '*.py')
 for filename in glob.glob(py_pattern):
+    print(filename)
     subprocess.call(['python', filename])
