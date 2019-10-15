49d6c4fecf23cc6e6ab68ca970c87bf4b7bab6af
Kip Hart <kiphart91@gmail.com>
2019-10-15 13:08:22 -0500

    added older python 3 versions to setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
