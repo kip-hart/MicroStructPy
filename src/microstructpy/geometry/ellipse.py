8722d1dd67e4ad6c607d717a31bf15d97299e1ff
Kip Hart <kiphart91@gmail.com>
2019-10-18 14:58:41 -0400

    copied ellipses.py from source, best_fit method calls this module instead
    Source: https://github.com/bdhammel/least-squares-ellipse-fitting

--- src/microstructpy/geometry/ellipse.py
+++ src/microstructpy/geometry/ellipse.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import patches
 
+from microstructpy.geometry import ellipses
+
 __author__ = 'Kenneth (Kip) Hart'
 
 
@@ -219,68 +221,11 @@ class Ellipse(object):
           (http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.1.7559&rep=rep1&type=pdf)
 
         """  # NOQA: E501
-        # Unpack the input points
-        pts = np.array(points, dtype='float')
-        x, y = pts.T
-
-        # Quadratic part of design matrix
-        D1 = np.mat(np.vstack([x * x, x * y, y * y])).T
-
-        # Linear part of design matrix
-        D2 = np.mat(np.vstack([x, y, np.ones(len(x))])).T
-
-        # Scatter matrix
-        S1 = D1.T * D1
-        S2 = D1.T * D2
-        S3 = D2.T * D2
-
-        # Constraint matrix
-        C1inv = np.mat([[0, 0, 0.5], [0, -1, 0], [0.5, 0, 0]])
-
-        # Reduced scatter matrix
-        M = C1inv * (S1 - S2 * S3.I * S2.T)
-
-        # Find eigenvalues
-        _, evec = np.linalg.eig(M)
-
-        # Mask
-        cond = 4 * np.multiply(evec[0, :], evec[2, :])
-        cond -= np.multiply(evec[1, :], evec[1, :])
-        a1 = evec[:, np.nonzero(cond.A > 0)[1]]
-
-        a2 = -S3.I * S2.T * a1
-
-        # Coefficients
-        a = a1[0, 0]
-        b = 0.5 * a1[1, 0]
-        c = a1[2, 0]
-
-        d = 0.5 * a2[0, 0]
-        f = 0.5 * a2[1, 0]
-        g = a2[2, 0]
-
-        # Center of ellipse
-        k = b * b - a * c
-        xc = (c * d - b * f) / k
-        yc = (a * f - b * d) / k
-
-        # Semi-axes lengths
-        numer = a * f * f
-        numer += c * d * d
-        numer += g * b * b
-        numer -= 2 * b * d * f
-        numer -= a * c * g
-        numer *= 2
-
-        tan2 = 2 * b / (a - c)
-        sq_val = np.sqrt(1 + tan2 * tan2)
-        denom1 = k * ((c - a) * sq_val - (c + a))
-        denom2 = k * ((a - c) * sq_val - (c + a))
-        width = np.sqrt(numer / denom1)
-        height = np.sqrt(numer / denom2)
-
-        # Angle of rotation
-        phi = 0.5 * np.arctan(tan2)
+        # Fit points using ellipses module
+        data = np.array(points).T
+        lsqe = ellipses.LSqEllipse()
+        lsqe.fit(data)
+        center, width, height, phi = lsqe.parameters()
 
         # Find pair closest to self
         s = np.sin(phi)
@@ -310,7 +255,7 @@ class Ellipse(object):
         ang_diff = np.arcsin(np.cross(x_ax_seed, x_ax_fit))
         ang_rad = self.angle_rad + ang_diff
 
-        return type(self)(center=(xc, yc), a=a, b=b, angle_rad=ang_rad)
+        return type(self)(center=center, a=a, b=b, angle_rad=ang_rad)
 
     # ----------------------------------------------------------------------- #
     # String and Representation Functions                                     #
