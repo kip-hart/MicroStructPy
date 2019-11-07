f649187de82b95843fef9b881e91d9e2d1a06205
Kip Hart <kiphart91@gmail.com>
2019-11-07 11:41:10 -0500

    bumped version

--- src/microstructpy/__init__.py
+++ src/microstructpy/__init__.py
@@ -5,5 +5,5 @@ import microstructpy.seeding
 import microstructpy.verification
 from microstructpy._vs import _ver_str
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __version__ = _ver_str(__version__)
