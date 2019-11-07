9d4a8ae6ec0d4e88f9435ef4dc85c64741a55246
Kip Hart <kiphart91@gmail.com>
2019-11-06 22:11:10 -0500

    prepared repo for NASA branch

--- src/microstructpy/__init__.py
+++ src/microstructpy/__init__.py
@@ -3,5 +3,7 @@ import microstructpy.geometry
 import microstructpy.meshing
 import microstructpy.seeding
 import microstructpy.verification
+from microstructpy._vs import _ver_str
 
 __version__ = '1.1.1'
+__version__ = _ver_str(__version__)
