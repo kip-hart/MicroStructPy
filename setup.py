29f1ce5e1f80a4edc45cb878767d00ae2d794983
Kip Hart <kiphart91@gmail.com>
2019-11-07 11:56:05 -0500

    updated setup and manifest for examples move

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    package_data={'': ['src/microstructpy/examples']},
+    package_data={'': ['src/microstructpy/examples/*.{csv,py,xml}',
+                       'src/microstructpy/examples/aluminum_micro.png']},
     include_package_data=True,
     zip_safe=False,
     classifiers=[
